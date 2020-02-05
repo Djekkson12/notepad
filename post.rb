99c4385c99ff7651befbaefc11aed77a6421dc58
Djekkson12 <53084473+Djekkson12@users.noreply.github.com>
2020-02-05 23:16:41 +0200

    some methods

--- post.rb
+++ post.rb
@@ -13,6 +13,7 @@ class Post
   def to_stings
 
 
+
   end
 
   def save
--- task.rb
+++ task.rb
@@ -1,3 +1,4 @@
+require 'date'
 class Task < Post
 
   def initialize
@@ -7,10 +8,22 @@ class Task < Post
   end
 
   def read_from_console
+    puts "Что мне сделать?"
+    @text = STDIN.gets.chomp
 
+    puts "К какому числу? Укажи дату в формате ДД.ММ.ГГГГ, например 12.05.1995"
+    input = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
   end
 
+
   def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y. %m.%d, %H:%M:%S")} \n\r \n\r"
+
+    deadline = "Срок: #{@due_date}"
+
+    return [deadline, @text, time_string]
 
   end
 
